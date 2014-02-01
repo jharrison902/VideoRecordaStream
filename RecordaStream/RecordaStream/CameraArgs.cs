dd721cec5e680f4b91bd352b28e8248fecc88c08
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-02-01 14:23:27 -0500

    Video and Audio Recording End
    -Created a project for recording audio and video on the user's end.
    Includes the following:
    -Camera capture class.
    -Audio capture via NAudio.

---
+++ RecordaStream/RecordaStream/CameraArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Camera
+{
+    public class CameraArgs : System.EventArgs
+    {
+        private System.Drawing.Image m_Image;
+        private ulong m_FrameNumber = 0;
+
+        public CameraArgs()
+        {
+        }
+
+        /// <summary>
+        ///  WebCamImage
+        ///  This is the image returned by the web camera capture
+        /// </summary>
+        public System.Drawing.Image WebCamImage
+        {
+            get
+            { return m_Image; }
+
+            set
+            { m_Image = value; }
+        }
+
+        /// <summary>
+        /// FrameNumber
+        /// Holds the sequence number of the frame capture
+        /// </summary>
+        public ulong FrameNumber
+        {
+            get
+            { return m_FrameNumber; }
+
+            set
+            { m_FrameNumber = value; }
+        }
+    }
+}
---
+++ RecordaStream/RecordaStream/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+namespace RecordaStream
+{
+    public partial class Form1 : Form
+    {
+        BufferedWaveProvider bwp;
+        WaveIn wi;
+        WaveOut wo;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cameraControl1.Size = new System.Drawing.Size(640, 480);
+            cameraControl1.Start(0, true);
+
+            wo = new WaveOut();
+            wi = new WaveIn();
+
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+
+            bwp = new BufferedWaveProvider(wi.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;
+
+            wo.Init(bwp);
+            wi.StartRecording();
+            wo.Play();
+        }
+
+        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void cameraControl1_ImageCaptured(object sender, Camera.CameraArgs e)
+        {
+            pictureBox1.Image = e.WebCamImage;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            wi.StopRecording();
+        }
+    }
+}
