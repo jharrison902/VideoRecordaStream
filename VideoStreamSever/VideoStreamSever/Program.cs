7dcad8bdd1067c7f073db2d24ba1462589164247
jharrison902 <jharrison902@gmail.com>
2014-02-01 15:06:32 -0500

    Server code?
    Maybe?

---
+++ VideoStreamSever/VideoStreamSever/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace VideoStreamSever
+{
+    class Program
+    {
+        static readonly int MAX_BUFFER = 2048;
+        static readonly int MAX_CONNECTIONS = 10;
+        static void Main(string[] args)
+        {
+            //byte[] dataBuffer = new byte[MAX_BUFFER];
+
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11337);
+
+            
+        }
+    }
+}
